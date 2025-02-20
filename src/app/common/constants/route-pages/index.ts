e061d19e97935d658d1530c5321bc99e06d10054
beefysalad <jpatrickzephyr@gmail.com>
2025-02-20 20:57:59 +0800

    feat: add auto sign in route to public routes

--- src/app/common/constants/route-pages/index.ts
+++ src/app/common/constants/route-pages/index.ts
@@ -3,10 +3,15 @@ export const ROUTES = {
   AUTH: {
     SIGN_UP: "/sign-up",
     SIGN_IN: "/sign-in",
+    API_AUTH_SIGN_IN: "/api/auth/signin",
   },
   USER: "/user",
 };
 
 export const PROTECTED_ROUTES = [ROUTES.USER];
 
-export const PUBLIC_ROUTES = [ROUTES.AUTH.SIGN_IN, ROUTES.AUTH.SIGN_UP];
+export const PUBLIC_ROUTES = [
+  ROUTES.AUTH.SIGN_IN,
+  ROUTES.AUTH.SIGN_UP,
+  ROUTES.AUTH.API_AUTH_SIGN_IN,
+];
