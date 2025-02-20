9dae48712c9c3caee57a61c287e0240a7acbea62
beefysalad <jpatrickzephyr@gmail.com>
2025-02-20 20:56:11 +0800

    feat: add redirection to authorized and non authorized users

--- src/app/common/constants/route-pages/index.ts
+++ src/app/common/constants/route-pages/index.ts
@@ -1,8 +1,12 @@
 export const ROUTES = {
-    ROOT: "/",
-    AUTH: {
-      SIGN_UP: "/sign-up",
-      SIGN_IN: "/sign-in"
-    },
-  };
-  
+  ROOT: "/",
+  AUTH: {
+    SIGN_UP: "/sign-up",
+    SIGN_IN: "/sign-in",
+  },
+  USER: "/user",
+};
+
+export const PROTECTED_ROUTES = [ROUTES.USER];
+
+export const PUBLIC_ROUTES = [ROUTES.AUTH.SIGN_IN, ROUTES.AUTH.SIGN_UP];
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,23 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./lib/auth";
-import { ROUTES } from "./app/common/constants/route-pages";
+import {
+  PROTECTED_ROUTES,
+  PUBLIC_ROUTES,
+  ROUTES,
+} from "./app/common/constants/route-pages";
 
-const protectedRoutes = ["/user"];
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const session = await auth();
 
-  if (protectedRoutes.some((route) => pathname.startsWith(route)) && !session) {
+  if (
+    PROTECTED_ROUTES.some((route) => pathname.startsWith(route)) &&
+    !session
+  ) {
     return NextResponse.redirect(new URL(ROUTES.AUTH.SIGN_IN, request.url));
   }
-  if (request.nextUrl.pathname.startsWith("/sign-up")) {
-    // This logic is only applied to /about
-    console.log("INTERCEPTED");
-  }
 
-  if (request.nextUrl.pathname.startsWith("/user")) {
-    // This logic is only applied to /dashboard
-    console.log("INTERCEPTED");
+  if (PUBLIC_ROUTES.some((route) => pathname.startsWith(route)) && session) {
+    return NextResponse.redirect(new URL(ROUTES.ROOT, request.url));
   }
 }
